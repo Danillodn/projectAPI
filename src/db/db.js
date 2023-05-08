7a0e61c4147f42602bbe2030f3d7bbe2c3d8bc3f
Danillodn <danillodiasnascimento@gmail.com>
2023-05-08 16:50:04 -0300

    mongodb connection creation

---
+++ src/db/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/adc-api', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+
+module.exports = mongoose;
