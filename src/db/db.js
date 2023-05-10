bfa1c67925b87ea10dfa179d8de10567cc4a03e9
Danillodn <danillodiasnascimento@gmail.com>
2023-05-10 13:13:12 -0300

    test

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, {
+  
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
   .then(() => {
     console.log('MongoDB connected');
+    console.log(process.env.MONGO_URI)
   })
   .catch(err => {
     console.error('Error connecting to MongoDB:', err);
