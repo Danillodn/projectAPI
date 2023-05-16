4f8b0908760a1ad1eaf6e23a8190b0999da96356
Danillo Dias Nascimento <132394923+Danillodn@users.noreply.github.com>
2023-05-16 19:31:17 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -56,14 +56,17 @@ app.get('/show_post/:post_id', async (req, res) => {
 })
 
 app.patch('/update_post/:post_id', async (req, res) => {
-    try {
-        const postId = req.params.post_id
-        const { title, description, content } = req.body
-        const post = await Post.findByIdAndUpdate(postId, { title, description, content }, { new: true})
-        res.send({ post })
-    } catch (err) {
-        res.status(200).send(err)
-    }
+//     try {
+//         const postId = req.params.post_id
+//         const { title, description, content } = req.body
+//         const post = await Post.findByIdAndUpdate(postId, { title, description, content }, { new: true})
+//         res.send({ post })
+//     } catch (err) {
+//         res.status(200).send(err)
+//     }
+    return(
+        "hello world"
+    )
 })
 
 
